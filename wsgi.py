c03abe0e526527a1aeddbbc954b5f85c5a922ba1
Zoya <zoyah768@gmail.com>
2024-04-29 17:30:47 +0530

    hostingchange

--- wsgi.py
+++ wsgi.py
@@ -6,7 +6,7 @@ socketio = SocketIO(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('home.html')
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
