f07c496a7eb710b759e590e4a7625d6179927696
Zoya <zoyah768@gmail.com>
2024-04-29 17:25:50 +0530

    hosting3

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,12 @@
-from app import app
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 if __name__ == '__main__':
     socketio.run(app, debug=True)
